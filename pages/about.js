f1d73cd64b7a9ecb4c461ac86fed854906794564
Vinicius <viniciusp.olivera@gmail.com>
2020-11-17 13:54:32 -0300

    Fixed Typo

--- pages/about.js
+++ pages/about.js
@@ -8,7 +8,7 @@ export default function About() {
             <header className={styles.header}>
                 <h1>_About</h1>
                 <p className={styles.about}>21 years old Computer Science student at Federal University of Paraíba(UFPB), currently working around with Python programming and Next.js Web Development. Open Source enthusiast.</p>
-                <p className={styles.about}>If you wish yo know more about my projects and ideas, check out my <Link href="/blog"><a>blog</a></Link>.</p>
+                <p className={styles.about}>If you wish to know more about my projects and ideas, check out my <Link href="/blog"><a>blog</a></Link>.</p>
             </header>
         </LayoutHome>
     )
